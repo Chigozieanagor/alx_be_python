ef2157fff6a80dd640588c1b383fddf693003988
chigozie <chigoanagor@gmail.com>
2025-07-10 04:31:35 +0100

    perform operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5
+sum1 = 10 + 5
+diff1 = 10 - 5
+pro1 = 10 * 5
+
+print("Addition of ", number1, "and", number2, "is", sum1)
+print("Subtraction of ", number1, "and", number2, "is", diff1)
+print("Multiplication of ", number1, "and", number2, "is", pro1)
