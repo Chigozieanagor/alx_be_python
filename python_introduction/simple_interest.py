0751db22adaace6a5c2814a0260ec03218fbdc7f
chigozie <chigoanagor@gmail.com>
2025-07-10 21:37:57 +0100

    correct calculations

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,5 +2,5 @@ P = principal = 1000
 R = rate = 0.05
 T = time = 3
 
-I = interest = P * R * T
-print("The simple interest is : ", I)
+interest = principal * rate * time
+print("The simple interest is : ", interest)
