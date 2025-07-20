817e62be9c63cffb0573c0e97f54e4fdbd58d6db
chigozie <chigoanagor@gmail.com>
2025-07-20 23:40:36 +0100

    redo match calculator

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,22 +1,22 @@
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
-operation =  input("chose the operation (+, -, *, /): ")
+operation =  input("Choose the operation (+, -, *, /): ")
 
 match operation:
     case "+":
         addition = num1 + num2
-        print(f"The result is {addition}")
+        print(f"The result is {addition}.")
     case "-":
         subtraction = num1 - num2
-        print(f"The result is {subtraction}")
+        print(f"The result is {subtraction}.")
     case "*":
         multiplication = num1 - num2
-        print(f"The result is {multiplication}")
+        print(f"The result is {multiplication}.")
     case "/":
         if num2 == 0:
-            print(f"Cannot divide by zero")
+            print(f"Cannot divide by zero.")
         else:
             division = num1 / num2
-            print(f"The result is {division}")
+            print(f"The result is {division}.")
     case _:
         print()
