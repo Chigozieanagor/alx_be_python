71b9fcf388847526557ad26e004c5cb250288caa
chigozie <chigoanagor@gmail.com>
2025-07-29 20:44:29 +0100

    correction3 explore datetime moudule

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 
 def display_current_datetime():
     current_date = datetime.now()
-    now_date = current_date.strftime("%Y-%m-%d : %H:%M:%S")
+    now_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
     print(f"Current date and time: {now_date}")
 
     return current_date
