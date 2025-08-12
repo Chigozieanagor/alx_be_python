9f71d0a95553f193924de06bc3e00e739e7618d2
chigozie <chigoanagor@gmail.com>
2025-08-12 23:08:44 +0100

    correct robust

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -4,7 +4,7 @@ def safe_divide(numerator, denominator):
         nume = float(numerator)
         deno = float(denominator)
         result = nume / deno
-        print(f"The result of the division is", end="")
+        print(f"The result of the division is ", end="")
         return result
     except ZeroDivisionError:
         print("Error: Cannot divide by zero.")
