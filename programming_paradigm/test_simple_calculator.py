3bf88d90e45f123a5d30c647a072ae5747d924b1
chigozie <chigoanagor@gmail.com>
2025-08-13 13:57:10 +0100

    correct calculator

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -14,12 +14,12 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(-1, -1), 0)
 
 
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(2, 3), 6)
         self.assertEqual(self.calc.multiply(-1, 5), -5)
 
 
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(2, 3), 5)
         self.assertEqual(self.calc.divide(-10, 2), -5)
 
